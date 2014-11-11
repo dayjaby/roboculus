527a4c67219e8edb03b6975357a2c193f934d98c
David Jablonski <djablonski@trave.mip.informatik.uni-kiel.de>
2014-11-11 15:50:11 +0100

    server client code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <VideoSource/VideoSource_Net.hh>
+#include <Base/Image/Image.hh>
+#include <Base/Image/ImageBase.hh>
+#include <Base/Image/ImageIO.hh>
+#include <Image/Camera.hh>
+#include <vector>
+#include <string>
+#include <Base/Image/ImageConvert.hh>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+int main(int argc, char**argv) {
+  BIAS::VideoSource_Net* cam = new BIAS::VideoSource_Net;
+  //BIAS::Image<float> greyimg(640,480,1,BIAS::ImageBase::ST_float);
+  BIAS::Camera<unsigned char> img;
+  std::cout << img.GetStorageType() << std::endl;
+  
+  cam->OpenDevice("powerbot",44887);
+  cam->CloseDevice();
+  cam->InitImage(img);
+  cam->PreGrab();
+  cam->GrabSingle(img);
+  cam->PostGrab();
+  cam->CloseDevice();
+  //BIAS::ImageConvert::ToGrey(img,greyimg);
+  BIAS::ImageIO::ExportLibJPEG("test.jpg",img);
+  return 0;
 }
